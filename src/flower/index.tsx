ec78183837b349bced94bda6ec8f7a8d89f4b419
swnb <swnblovego@gmail.com>
2021-02-25 12:08:06 +0800

    feat(flower): add props scale

--- src/flower/index.tsx
+++ src/flower/index.tsx
@@ -1,20 +1,36 @@
+import { CSSProperties, useMemo } from "react"
 import styles from "./index.module.scss"
 
-function Flower() {
+interface IFlowerProps {
+  scale?: number
+  style?: CSSProperties
+}
+
+function Flower({ scale = 1, style }: IFlowerProps) {
+  const containerStyle = useMemo(
+    () => ({
+      ...style,
+      transform: `scale3d(${scale},${scale},${scale})`,
+    }),
+    [scale, style]
+  )
+
   return (
-    <div className={styles.box}>
-      {Array(6)
-        .fill(undefined)
-        .map((_, index) => (
-          <div key={index} id={`item${index}`}>
-            <div>
-              <div />
-              <div />
-              <div />
-              <div />
+    <div className={styles.container} style={containerStyle}>
+      <div className={styles.box}>
+        {Array(6)
+          .fill(undefined)
+          .map((_, index) => (
+            <div key={index} id={`item${index}`}>
+              <div>
+                <div />
+                <div />
+                <div />
+                <div />
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+      </div>
     </div>
   )
 }
